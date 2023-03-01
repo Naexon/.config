a49e7c87a934c6f4c02afca5af101b498f518af4
Naexon <a.woda@outlook.com>
2023-03-01 22:04:02 +0100

    added neofetch changed shell open to run neofetch

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,11 @@ set fish_greeting
 
 # Show nice color palatte images on shell open
 # See https://gitlab.com/dwt1/shell-color-scripts
-colorscript random
+# colorscript random
+
+# Runs neofetch on shell open
+echo 
+neofetch
 
 # Tell fish to use starship promt
 starship init fish | source
